c3aed320a224d0eb29553632c26a83c977b27b51
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-10-07 15:15:05 +0800

    feat: create add comodity function and print document function

--- renderer/components/SideBar.tsx
+++ renderer/components/SideBar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import Moment from 'react-moment'
 
 type TypeProps = {
   items: any
   setItem: any
+  setSingleItem: any
 }
 
 interface FormData {
@@ -14,13 +16,23 @@ interface FormData {
   weights_measurements: string
   unit_value: string
   total_value: string
+
+  use_of_goods_in_the_zone: string
+  currency_payment_will_be_made: string
+  name_address_supplier: string
+  date_delivery_zone: string
+  carrier: string
+  zone_enterprise_registration_no: string
+  permission_hereby_granted: string
+  permit_date: string
+  delivery_receipt_no: string
 }
 
-const SideBar: React.FC< TypeProps> = ({ items, setItem }) => {
+const SideBar: React.FC<TypeProps> = ({ items, setItem, setSingleItem }) => {
 
-  const { handleSubmit, register, reset, formState: { errors, isSubmitting }} = useForm()
+  const { handleSubmit, register, reset } = useForm()
 
-  function toSubmit(formData: FormData) {
+  function addCommodity(formData: FormData) {
     const pkgs_unit = formData.pkgs_unit
     const marks_numbers = formData.marks_numbers
     const description_commodities = formData.description_commodities
@@ -28,6 +40,19 @@ const SideBar: React.FC< TypeProps> = ({ items, setItem }) => {
     const weights_measurements = formData.weights_measurements
     const unit_value = formData.unit_value
     const total_value = formData.total_value
+    const use_of_goods_in_the_zone = formData.use_of_goods_in_the_zone
+    const currency_payment_will_be_made = formData.currency_payment_will_be_made
+    const name_address_supplier = formData.name_address_supplier
+    const date_delivery_zone = formData.date_delivery_zone
+    const carrier = formData.carrier
+    const zone_enterprise_registration_no = formData.zone_enterprise_registration_no
+    const permission_hereby_granted = formData.permission_hereby_granted
+    const permit_date = formData.permit_date
+    const delivery_receipt_no = formData.delivery_receipt_no
+
+    let format= (d,a=d.toString().split` `)=> a[2]+"-"+a[1]+"-"+a[3]
+
+    const getdate = format(Date())
 
     const initialData = [{
       pkgs_unit: pkgs_unit,
@@ -40,78 +65,167 @@ const SideBar: React.FC< TypeProps> = ({ items, setItem }) => {
     }]
 
     setItem([...items, ...initialData])
-    reset()
+
+    setSingleItem({
+      getdate: getdate,
+      use_of_goods_in_the_zone: use_of_goods_in_the_zone,
+      currency_payment_will_be_made: currency_payment_will_be_made,
+      name_address_supplier: name_address_supplier,
+      date_delivery_zone: date_delivery_zone,
+      carrier: carrier,
+      zone_enterprise_registration_no: zone_enterprise_registration_no,
+      permission_hereby_granted: permission_hereby_granted,
+      delivery_receipt_no: delivery_receipt_no
+    })
+
+    reset({
+      use_of_goods_in_the_zone,
+      currency_payment_will_be_made,
+      name_address_supplier,
+      date_delivery_zone,
+      carrier,
+      zone_enterprise_registration_no,
+      permission_hereby_granted,
+      permit_date,
+      delivery_receipt_no
+    })
   }
 
   return (
-    <div className="flex flex-col w-full px-3 py-5 space-y-5">
-      <div className="font-bold text-sm">SPMI FRAGMENT</div>
-      <form onSubmit={handleSubmit(toSubmit)} className="flex flex-col w-full space-y-3">
-        <div className="flex flex-col w-full space-y-1">
-          <label className="px-1 text-xs text-white text-opacity-50">No. of Pkgs. Units</label>
-          <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-            <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('pkgs_unit', { required: true })} />
+    <form onSubmit={handleSubmit(addCommodity)} className="flex flex-row items-center w-full h-full">     
+      <div className="flex flex-col items-start w-full h-full overflow-y-auto px-3 py-5 space-y-5">
+        <div className="font-bold text-sm">SPMI FRAGMENT (Single Data)</div>
+        <div className="flex flex-col w-full space-y-3">
+          <div className="flex flex-row items-center w-full space-x-2">
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Use of goods in the zone</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('use_of_goods_in_the_zone', { required: true })} />
+              </div>
+            </div>
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Currency in which payment will be made</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('currency_payment_will_be_made', { required: true })} />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col w-full space-y-1">
-          <label className="px-1 text-xs text-white text-opacity-50">Marks & Numbers</label>
-          <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-            <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('marks_numbers', { required: true })} />
-          </div>
-        </div>
-        <div className="flex flex-col w-full space-y-1">
-          <label className="px-1 text-xs text-white text-opacity-50">Description of Commodities</label>
-          <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-            <textarea className="w-full p-3 text-sm bg-transparent outline-none resize-none" rows={4} {...register('description_commodities', { required: true })} />
-          </div>
-        </div>
-        <div className="flex flex-row items-center w-full space-x-2"> 
-          <div className="flex flex-col w-full space-y-1">
-            <label className="px-1 text-xs text-white text-opacity-50">Quantity</label>
-            <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-              <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('quantity', { required: true })} />
+          <div className="flex flex-row items-center w-full space-x-2">
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Name and Address of Supplier</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('name_address_supplier', { required: true })} />
+              </div>
+            </div>
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Date of Delivery to the Zone</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('date_delivery_zone', { required: true })} />
+              </div>
+            </div>
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Carrier</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('carrier', { required: true })} />
+              </div>
             </div>
           </div>
           <div className="flex flex-col w-full space-y-1">
-            <label className="px-1 text-xs text-white text-opacity-50">Weights/Measurements</label>
+            <label className="px-1 text-xs text-white text-opacity-50">Zone Enterprise & Registration No.</label>
             <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-              <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('weights_measurements', { required: true })} />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-row items-center w-full space-x-2"> 
-          <div className="flex flex-col w-full space-y-1">
-            <label className="px-1 text-xs text-white text-opacity-50">Unit Value</label>
-            <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-              <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('unit_value', { required: true })} />
+              <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('zone_enterprise_registration_no', { required: true })} />
             </div>
           </div>
           <div className="flex flex-col w-full space-y-1">
-            <label className="px-1 text-xs text-white text-opacity-50">Total Value</label>
+            <label className="px-1 text-xs text-white text-opacity-50">Persmission is hereby granted to bring into the</label>
             <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
-              <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('total_value', { required: true })} />
+              <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('permission_hereby_granted', { required: true })} />
+            </div>
+          </div>
+          <div className="flex flex-row items-center w-full space-x-2">
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Delivery Receipt No.</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('delivery_receipt_no', { required: true })} />
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col w-full space-y-2">
-          <button
-            className="w-full p-3 text-sm rounded-lg bg-gray-800 hover:bg-opacity-70"
-            type="submit"
-          >
-            Add Commodity
-          </button>
-          <button
-            className="w-full p-3 text-sm rounded-lg bg-gray-800 hover:bg-opacity-70"
-            type="button"
-            onClick={() => {
-              setItem([])
-            }}
-          >
-            Clear All Commodities
-          </button>
+      </div>
+
+      <div className="flex flex-col items-start w-full max-w-xl h-full overflow-y-auto px-3 py-5 space-y-5">
+        <div className="font-bold text-sm">SPMI FRAGMENT (Data List)</div>
+        <div className="flex flex-col w-full space-y-3">
+          <div className="flex flex-row items-center w-full space-x-2">
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">No. of Pkgs. Units</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('pkgs_unit', { required: true })} />
+              </div>
+            </div>
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Marks & Numbers</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('marks_numbers', { required: true })} />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-full space-y-1">
+            <label className="px-1 text-xs text-white text-opacity-50">Description of Commodities</label>
+            <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+              <textarea className="w-full p-3 text-sm bg-transparent outline-none resize-none" rows={3} {...register('description_commodities', { required: true })} />
+            </div>
+          </div>
+          <div className="flex flex-row items-center w-full space-x-2"> 
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Quantity</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('quantity', { required: true })} />
+              </div>
+            </div>
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Weights/Measurements</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('weights_measurements', { required: true })} />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-row items-center w-full space-x-2"> 
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Unit Value</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('unit_value', { required: true })} />
+              </div>
+            </div>
+            <div className="flex flex-col w-full space-y-1">
+              <label className="px-1 text-xs text-white text-opacity-50">Total Value</label>
+              <div className="flex items center w-full rounded-lg bg-gray-800 border border-gray-800 focus-within:border-gray-400">
+                <input type="text" className="w-full p-3 bg-transparent outline-none" {...register('total_value', { required: true })} />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-full space-y-2">
+            <button
+              className="w-full p-3 text-sm text-white text-opacity-70 rounded-lg bg-gray-800 hover:bg-opacity-70"
+              type="submit"
+            >
+              Add Commodity
+            </button>
+            <button
+              className="w-full p-3 text-sm text-white text-opacity-70 rounded-lg bg-gray-800 hover:bg-opacity-70"
+              type="button"
+              onClick={() => {
+                setItem([])
+                setSingleItem([])
+                reset()
+              }}
+            >
+              Clear All
+            </button>
+          </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </form>
   )
 }
 
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,41 +1,45 @@
 import type { NextPage } from 'next'
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 import SideBar from '../components/SideBar'
+import PrintPreview from '../components/PrintPreview'
 
 const Home: NextPage = () => {
   
   const [items, setItem] = React.useState<any>([])
+  const [singleItem, setSingleItem] = React.useState<any>([])
 
   return (
     <React.Fragment>
       <Head>
         <title>SPMI Fragment</title>
       </Head>
-      <div className="flex w-full h-screen bg-gray-700 text-white">
-        <div className="flex flex-col w-full max-w-xs h-full overflow-y-auto border-r border-black border-opacity-10">
-          <SideBar items={items} setItem={setItem} />
+      <div className="flex flex-col w-full h-screen cursor-default bg-gray-700 text-white">
+        <div className="flex flex-col w-full max-w-full h-full overflow-hidden border-b border-black border-opacity-10">
+          <SideBar items={items} setItem={setItem} setSingleItem={setSingleItem} />
         </div>
         <div className="flex flex-col w-full max-w-full h-full overflow-y-auto">
           <div className="flex flex-col w-full p-2">
-            <div className="flex items-center w-full px-5 py-3">
-              <h1 className="font-bold text-sm">List of Commodities</h1>
+            <div className="flex items-center justify-between w-full px-5 py-3">
+              <h1 className="font-bold text-xl">List of Commodities</h1>
+              <PrintPreview items={items} singleItem={singleItem} />
             </div>
             <table className="table-auto p-10 border border-black border-opacity-10">
               <thead className="border border-black border-opacity-10">
                 <tr>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">No. of Pkgs. Units</th>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">Marks & Numbers</th>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">Description of Commodities</th>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">Quantity</th>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">Weights/Measurements</th>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">Unit Value</th>
-                  <th className="p-3 text-sm text-blue-400 border border-black border-opacity-10">Total Value</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">No. of Pkgs. Units</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">Marks & Numbers</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">Description of Commodities</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">Quantity</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">Weights/Measurements</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">Unit Value</th>
+                  <th className="p-3 text-sm text-blue-300 border border-black border-opacity-10">Total Value</th>
                 </tr>
               </thead>
               <tbody>
                 {items.map((item: any, i: number) => (
-                  <tr className="border border-black border-opacity-10" key={i}>
+                  <tr className="border border-black border-opacity-10 hover:bg-gray-800 hover:bg-opacity-50" key={i}>
                     <td className="px-5 py-3 text-sm text-gray-100 border border-black border-opacity-10">{item.pkgs_unit}</td>
                     <td className="px-5 py-3 text-sm text-gray-100 border border-black border-opacity-10">{item.marks_numbers}</td>
                     <td className="px-5 py-3 text-sm text-gray-100 border border-black border-opacity-10">{item.description_commodities}</td>
@@ -47,6 +51,55 @@ const Home: NextPage = () => {
                 ))}
               </tbody>
             </table>
+            <div className={`${singleItem.length !== 0 ? 'flex' : 'hidden'} flex-col w-full px-1 border-t border-black border-opacity-10 mt-5`}>
+              <div className="flex w-full px-5 py-3">
+                <h1 className="font-bold text-xl">Other Data</h1>
+              </div>
+              <div className="flex flex-col w-full space-y-2">
+                <div className="flex flex-row items-center w-full space-x-2">
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Use of good in the Zone</span>
+                    <span>{ singleItem.use_of_goods_in_the_zone }</span>
+                  </div>
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Currency in which payment will be made</span>
+                    <span>{ singleItem.currency_payment_will_be_made }</span>
+                  </div>
+                </div>
+                <div className="flex flex-row items-center w-full space-x-2">
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Name and Address of Supplier</span>
+                    <span>{ singleItem.name_address_supplier }</span>
+                  </div>
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Date of Delivery to the Zone</span>
+                    <span>{ singleItem.date_delivery_zone }</span>
+                  </div>
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Carrier</span>
+                    <span>{ singleItem.carrier }</span>
+                  </div>
+                </div>
+                <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                  <span className="flex w-full font-light text-xs text-white text-opacity-50">Zone Enterprise & Registration No.</span>
+                  <span>{ singleItem.zone_enterprise_registration_no }</span>
+                </div>
+                <div className="flex flex-row items-center w-full space-x-2">
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Persmission is hereby granted to bring into the</span>
+                    <span>{ singleItem.permission_hereby_granted }</span>
+                  </div>
+                  <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                    <span className="flex w-full font-light text-xs text-white text-opacity-50">Permit Date</span>
+                    <span>{ singleItem.getdate }</span>
+                  </div>
+                </div>
+                <div className="flex flex-col w-full p-3 rounded-lg border border-black border-opacity-10 bg-gray-800 space-y-1">
+                  <span className="flex w-full font-light text-xs text-white text-opacity-50">Delivery Receipt No.</span>
+                  <span>{ singleItem.delivery_receipt_no }</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
